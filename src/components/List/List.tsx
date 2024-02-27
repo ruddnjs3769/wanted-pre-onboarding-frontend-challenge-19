cfe76941a10a4beed79e43fb95a691e48e2e2b1c
KimGyeongwon <ruddnjs6651@gmail.com>
2024-02-27 21:44:51 +0900

    feat: list 스크롤 작업, style: list 스타일 작업

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,16 +1,39 @@
 import { useSelector } from "react-redux";
 import ListItem from "./ListItem";
 import { RootState } from "@/store/store";
+import logo from "@/assets/logo.png";
+import { useState } from "react";
 
 const List: React.FC = () => {
   const list = useSelector((state: RootState) => state.list);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = (e) => {
+    setIsScrolled(e.target.scrollTop > 0);
+  };
 
   return (
-    <div>
-      {list.map((item, index) => (
-        <ListItem key={index} item={item} />
-      ))}
-    </div>
+    <section className="w-full">
+      {/* 리스트 카운트 */}
+      <figure className="flex justify-between items-center mb-6">
+        <div className="w-8 h-8 bg-black text-white flex justify-center items-center rounded-full">
+          {list.length}
+        </div>
+        <span className="h-0 w-[245px] border-b border-black " />
+        <img src={logo} alt="logo" className="w-11 h-5 ml-2" />
+      </figure>
+      {/* 리스트들 */}
+      <article
+        className={`flex flex-col gap-2 h-[75vh] overflow-y-scroll scrollbar-hide transition-all duration-300 ease-in-out ${
+          isScrolled ? "shadow-inner" : "shadow-none"
+        }`}
+        onScroll={handleScroll}
+      >
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))}
+      </article>
+    </section>
   );
 };
 
--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -14,9 +14,14 @@ const ListItem: React.FC<ListItemProps> = ({ item }) => {
   };
 
   return (
-    <div>
+    <div className="w-full h-7 border border-[#c9c9c9] p-2 flex justify-between items-center">
       {item}
-      <button onClick={handleDelete}>Delete</button>
+      <button
+        className="w-[60px] h-5 bg-[#2B9F45] rounded-full text-white text-xs font-bold hover:bg-white hover:text-[#2B9F45] hover:border hover:border-[#2b9f45] transition-all duration-300 ease-in-out"
+        onClick={handleDelete}
+      >
+        Done!
+      </button>
     </div>
   );
 };
